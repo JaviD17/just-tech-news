d711befeaf4cb615debcb496c8510b1dac5c6664
Javier Dominguez II <Javi7057@gmail.com>
2021-08-08 21:37:48 -0500

    all routes working and tested

--- config/connection.js
+++ config/connection.js
@@ -2,7 +2,7 @@ const Sequelize = require('sequelize');
 
 require('dotenv').config();
 
-const sequelize = new Sequelize('just_tech_news_db', 'root', '7171reivaJJ!!', {
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
     host: 'localhost',
     dialect: 'mysql',
     port: 3306
