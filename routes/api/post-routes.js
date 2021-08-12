bb92c57382ccb4d7e2cc9f46d407eb0157679eba
Javier Dominguez II <Javi7057@gmail.com>
2021-08-12 10:55:26 -0500

    testing new vote route with insomnia core, coming backk when connection is more stable

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { Post, User } = require('../../models/');
+const { Post, User, Vote } = require('../../models/');
 
 // get all users
 router.get('/', (req, res) => {
@@ -63,7 +63,12 @@ router.post('/', (req, res) => {
 
 // PUT /api/posts/upvote
 router.put('/upvote', (req, res) => {
-
+    Vote.create({
+        user_id: req.body.user_id,
+        post_id: req.body.post_id
+    })
+        .then(dbPostData => (dbPostData))
+        .catch(err => res.json(err));
 });
 
 router.put('/:id', (req, res) => {
